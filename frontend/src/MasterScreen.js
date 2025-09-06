ae9a260991c74c36844e2af4b7269c36fe5721e8
Yash <garg.yashvardhan@gmail.com>
2025-09-06 15:55:46 -0700

    added MasterScreen for moderation

---
+++ frontend/src/MasterScreen.js
@@ -0,0 +1,2 @@
+// MasterScreen.js allows for super user (manager) to manage the database, delete users, posts, and courses.
+// It is a separate component that is only accessible to the super user.
